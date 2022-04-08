6fd5bb1ada2d01c04f4cde5232ba4d55e8d6cb4e
Oluwapelumi Ajayi <pelumi.ajayi@iota.org>
2022-04-08 10:36:01 +0100

    Burn native tokens without foundry

--- src/account/operations/transfer/high_level/burn_native_token.rs
+++ src/account/operations/transfer/high_level/burn_native_token.rs
@@ -1,6 +1,8 @@
 // Copyright 2022 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
+use std::cell::RefCell;
+
 use crate::{
     account::{
         handle::AccountHandle,
@@ -14,8 +16,8 @@ use iota_client::bee_message::{
     address::AliasAddress,
     output::{
         unlock_condition::{ImmutableAliasAddressUnlockCondition, UnlockCondition},
-        AliasOutputBuilder, FoundryId, FoundryOutputBuilder, NativeToken, Output, SimpleTokenScheme, TokenId,
-        TokenScheme,
+        AliasOutputBuilder, BasicOutputBuilder, FoundryId, FoundryOutputBuilder, NativeToken, NftOutputBuilder, Output,
+        SimpleTokenScheme, TokenId, TokenScheme, OUTPUT_COUNT_MAX,
     },
 };
 use primitive_types::U256;
@@ -75,8 +77,10 @@ impl AccountHandle {
         drop(account);
 
         if let Output::Alias(alias_output) = &existing_alias_output_data.output {
+            // Amount can't be burned, only native tokens
+            let amount = existing_alias_output_data.amount + existing_foundry_output.amount();
             // Create the new alias output with the same feature blocks, just updated state_index
-            let mut new_alias_output_builder = AliasOutputBuilder::new(existing_alias_output_data.amount, alias_id)?
+            let mut new_alias_output_builder = AliasOutputBuilder::new(amount, alias_id)?
                 .with_state_index(alias_output.state_index() + 1)
                 .with_foundry_counter(alias_output.foundry_counter());
 
@@ -101,7 +105,7 @@ impl AccountHandle {
                         foundry_id.serial_number(),
                         token_id.token_tag(),
                         TokenScheme::Simple(SimpleTokenScheme::new(
-                            foundry_simple_ts.circulating_supply(),
+                            *foundry_simple_ts.minted_tokens(),
                             foundry_simple_ts.melted_tokens() + burn_token_amount,
                             *foundry_simple_ts.maximum_supply(),
                         )?),
@@ -122,6 +126,77 @@ impl AccountHandle {
         }
     }
 
+    /// Function to burn native tokens without foundry
+    pub async fn burn_native_token_without_foundry(
+        &self,
+        burn_native_token_options: BurnNativeTokenOptions,
+        options: Option<TransferOptions>,
+    ) -> crate::Result<TransferResult> {
+        log::debug!("[TRANSFER] burn_native_token_without_foundry");
+        let token_id = burn_native_token_options.native_token.0;
+        let burn_token_amount = burn_native_token_options.native_token.1;
+
+        let account = self.read().await;
+        let mut inputs_and_outputs = Vec::new();
+        for (output_id, output_data) in account.unspent_outputs().iter() {
+            if let Some(native_tokens) = output_data.output.native_tokens() {
+                let amount = RefCell::new(U256::from(0));
+                let not_to_be_burnt_native_tokens = native_tokens
+                    .iter()
+                    .filter(|native_token| {
+                        if *native_token.token_id() == token_id {
+                            amount.replace_with(|current| *current + *native_token.amount());
+                            return false;
+                        }
+                        true
+                    })
+                    .cloned();
+
+                // If the output has a native token that we wish to burn,
+                // clone the output but without native tokens that are to be burnt
+                if !amount.borrow().is_zero() {
+                    let output = AccountHandle::rebuild_output_with_native_tokens(
+                        &output_data.output,
+                        not_to_be_burnt_native_tokens,
+                    )?;
+                    inputs_and_outputs.push((output_id, *amount.borrow(), output));
+                }
+            }
+        }
+
+        // Sort descending order
+        inputs_and_outputs.sort_by(|a, b| b.1.cmp(&a.1));
+
+        // Select unspent outputs with token id that sum up to the required amount
+        let mut outputs = Vec::new();
+        let mut custom_inputs = Vec::new();
+        let mut native_token_amount_accumulator = U256::from(0);
+        for input_and_output in inputs_and_outputs.into_iter().take(OUTPUT_COUNT_MAX as usize) {
+            if native_token_amount_accumulator >= burn_token_amount {
+                break;
+            }
+            outputs.push(input_and_output.2);
+            custom_inputs.push(*input_and_output.0);
+            native_token_amount_accumulator += input_and_output.1;
+        }
+
+        drop(account);
+
+        let options = match options {
+            Some(mut options) => {
+                options.custom_inputs.replace(custom_inputs);
+                Some(options)
+            }
+            None => Some(TransferOptions {
+                custom_inputs: Some(custom_inputs),
+                ..Default::default()
+            }),
+        };
+
+        // TODO: Avoid or work on `try_select_inputs` to make it burn native tokens rather than have them put in remainder
+        self.send(outputs, options).await
+    }
+
     /// Function to destroy foundry
     pub async fn destroy_foundry(
         &self,
@@ -129,7 +204,6 @@ impl AccountHandle {
         options: Option<TransferOptions>,
     ) -> crate::Result<TransferResult> {
         log::debug!("[TRANSFER] destroy_foundry");
-        let byte_cost_config = self.client.get_byte_cost_config().await?;
 
         let alias_id = *foundry_id.alias_address().alias_id();
         let account = self.read().await;
@@ -155,16 +229,18 @@ impl AccountHandle {
             .ok_or_else(|| Error::BurningFailed("Required alias output for foundry not found".to_string()))?
             .clone();
 
-        let existing_foundry_output = existing_foundry_output
+        let existing_foundry_output_data = existing_foundry_output
             .ok_or_else(|| Error::BurningFailed("Required foundry output not found".to_string()))?
             .clone();
 
         drop(account);
 
-        let custom_inputs = vec![existing_alias_output_data.output_id, existing_foundry_output.output_id];
+        let custom_inputs = vec![
+            existing_alias_output_data.output_id,
+            existing_foundry_output_data.output_id,
+        ];
         let options = match options {
             Some(mut options) => {
-                // TODO: Find out if there would be a reason to append to the user provided custom inputs?
                 options.custom_inputs.replace(custom_inputs);
                 Some(options)
             }
@@ -176,11 +252,12 @@ impl AccountHandle {
 
         let outputs = match existing_alias_output_data.output {
             Output::Alias(alias_output) => {
+                // Amount can't be burned, only native tokens
+                let amount = existing_alias_output_data.amount + existing_foundry_output_data.amount;
                 // Create the new alias output with the same feature blocks, just updated state_index
-                let mut new_alias_output_builder =
-                    AliasOutputBuilder::new(existing_alias_output_data.amount, alias_id)?
-                        .with_state_index(alias_output.state_index() + 1)
-                        .with_foundry_counter(alias_output.foundry_counter());
+                let mut new_alias_output_builder = AliasOutputBuilder::new(amount, alias_id)?
+                    .with_state_index(alias_output.state_index() + 1)
+                    .with_foundry_counter(alias_output.foundry_counter());
 
                 for unlock_condition in alias_output.unlock_conditions().clone().into_iter() {
                     new_alias_output_builder = new_alias_output_builder.add_unlock_condition(unlock_condition);
@@ -200,5 +277,61 @@ impl AccountHandle {
         };
 
         self.send(outputs, options).await
+    }
+
+    // This should preferably be done in the bee crate to make sure that
+    // potentially new output fields are not left out while cloning
+    pub(crate) fn rebuild_output_with_native_tokens(
+        output: &Output,
+        native_tokens: impl IntoIterator<Item = NativeToken>,
+    ) -> crate::Result<Output> {
+        let output = match output {
+            Output::Basic(basic_output) => Output::Basic(
+                BasicOutputBuilder::new(basic_output.amount())?
+                    .with_native_tokens(native_tokens)
+                    .with_unlock_conditions(basic_output.unlock_conditions().iter().cloned())
+                    .with_feature_blocks(basic_output.feature_blocks().iter().cloned())
+                    .finish()?,
+            ),
+            Output::Alias(alias_output) => Output::Alias(
+                AliasOutputBuilder::new(alias_output.amount(), *alias_output.alias_id())?
+                    .with_native_tokens(native_tokens)
+                    .with_state_index(alias_output.state_index())
+                    .with_state_metadata(alias_output.state_metadata().to_vec())
+                    .with_foundry_counter(alias_output.foundry_counter())
+                    .with_unlock_conditions(alias_output.unlock_conditions().iter().cloned())
+                    .with_feature_blocks(alias_output.feature_blocks().iter().cloned())
+                    .with_immutable_feature_blocks(alias_output.immutable_feature_blocks().iter().cloned())
+                    .finish()?,
+            ),
+            Output::Foundry(foundry_output) => Output::Foundry(
+                FoundryOutputBuilder::new(
+                    foundry_output.amount(),
+                    foundry_output.serial_number(),
+                    *foundry_output.token_tag(),
+                    foundry_output.token_scheme().clone(),
+                )?
+                .with_native_tokens(native_tokens)
+                .with_unlock_conditions(foundry_output.unlock_conditions().iter().cloned())
+                .with_feature_blocks(foundry_output.feature_blocks().iter().cloned())
+                .with_immutable_feature_blocks(foundry_output.immutable_feature_blocks().iter().cloned())
+                .finish()?,
+            ),
+            Output::Nft(nft_output) => Output::Nft(
+                NftOutputBuilder::new(nft_output.amount(), *nft_output.nft_id())?
+                    .with_native_tokens(native_tokens)
+                    .with_unlock_conditions(nft_output.unlock_conditions().iter().cloned())
+                    .with_feature_blocks(nft_output.feature_blocks().iter().cloned())
+                    .with_immutable_feature_blocks(nft_output.immutable_feature_blocks().iter().cloned())
+                    .finish()?,
+            ),
+            Output::Treasury(_) => {
+                return Err(crate::Error::InvalidOutputKind(
+                    "Treasury output cannot hold native tokens".to_string(),
+                ))
+            }
+        };
+
+        Ok(output)
     }
 }
